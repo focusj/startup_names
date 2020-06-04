0feb7b46804faa3939e74c7270a91e3bd5fae03c
wangkang.kangwang <wangkang.kangwang@bytedance.com>
2020-06-04 09:59:03 +0800

    extract start up app to a standalone dart file
    Change-Id: I2aa41ddc28290939a86c4bd40cb78c9d74deb347

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,6 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:startups/startups.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:startups/main.dart';
@@ -13,7 +14,7 @@ import 'package:startups/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(StartupNamesApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
